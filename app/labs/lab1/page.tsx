0f72b0fc98aa221e179022d7d9ca9d6b43024a26
Olivia Hill <hill.ol@northeastern.edu>
2026-01-11 14:25:44 -0500

    adding more to the form component and link

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 export default function Lab1() {
   return (
     <div id="wd-lab1">
@@ -149,7 +151,78 @@ export default function Lab1() {
                         id="wd-all-good">
                 Hello World!
                 </button>
-                
+                <h5 id="wd-radio-buttons">Radio buttons</h5>
+                <label>Favorite movie genre:</label><br />
+
+                <input type="radio" name="radio-genre" id="wd-radio-comedy"/>
+                <label htmlFor="wd-radio-comedy">Comedy</label><br />
+
+                <input type="radio" name="radio-genre" id="wd-radio-drama"/>
+                <label htmlFor="wd-radio-drama">Drama</label><br />
+                <input type="radio" name="radio-genre" id="wd-radio-scifi"/>
+                <label htmlFor="wd-radio-scifi">Science Fiction</label><br />
+                <input type="radio" name="radio-genre" id="wd-radio-fantasy"/>
+                <label htmlFor="wd-radio-fantasy">Fantasy</label>
+
+                <h5 id="wd-checkboxes">Checkboxes</h5>
+                <label>Favorite movie genre:</label><br/>
+
+                <input type="checkbox" name="check-genre" id="wd-chkbox-comedy"/>
+                <label htmlFor="wd-chkbox-comedy">Comedy</label><br/>
+
+                <input type="checkbox" name="check-genre" id="wd-chkbox-drama"/>
+                <label htmlFor="wd-chkbox-drama">Drama</label><br/>
+
+                <input type="checkbox" name="check-genre" id="wd-chkbox-scifi"/>
+                <label htmlFor="wd-chkbox-scifi">Science Fiction</label><br/>
+
+                <input type="checkbox" name="check-genre" id="wd-chkbox-fantasy"/>
+                <label htmlFor="wd-chkbox-fantasy">Fantasy</label>
+
+                <h4 id="wd-dropdowns">Dropdowns</h4>
+
+                <h5>Select one</h5>
+                <label  htmlFor="wd-select-one-genre"> Favorite movie genre: </label><br/>
+                <select id="wd-select-one-genre">
+                    <option value="COMEDY">Comedy</option>
+                    <option value="DRAMA">Drama</option>
+                    <option selected value="SCIFI">Science Fiction</option>
+                    <option value="FANTASY">Fantasy</option>
+                </select>
+
+                <h5>Select many</h5>
+                <label  htmlFor="wd-select-many-genre"> Favorite movie genres: </label><br/>
+                <select multiple id="wd-select-many-genre">
+                    <option value="COMEDY" selected> Comedy          </option>
+                    <option value="DRAMA">           Drama           </option>
+                    <option value="SCIFI"  selected> Science Fiction </option>
+                    <option value="FANTASY">         Fantasy         </option>
+                </select>
+
+                <h4>Other HTML field types</h4>
+
+                <label htmlFor="wd-text-fields-email"> Email: </label>
+                <input type="email"
+                    placeholder="jdoe@somewhere.com"
+                    id="wd-text-fields-email"/><br/>
+
+                <label htmlFor="wd-text-fields-salary-start"> Starting salary:</label>
+                <input type="number"
+                    defaultValue="100000"
+                    placeholder="1000"
+                    id="wd-text-fields-salary-start"/><br/>
+
+                <label htmlFor="wd-text-fields-rating"> Rating: </label>
+                <input type="range"
+                    defaultValue="4"
+                    max="5"
+                    placeholder="Doe"
+                    id="wd-text-fields-rating"/><br/>
+
+                <label htmlFor="wd-text-fields-dob"> Date of birth: </label>
+                <input type="date"
+                    defaultValue="2000-01-21"
+                    id="wd-text-fields-dob"/><br/>
             </form>
         </div>
     </div>

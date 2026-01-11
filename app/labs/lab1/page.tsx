ec0e924e8dcb34d6454f3b5046ab37a6dad7298f
Olivia Hill <hill.ol@northeastern.edu>
2026-01-11 14:17:23 -0500

    adding form components

--- app/labs/lab1/page.tsx
+++ app/labs/lab1/page.tsx
@@ -140,7 +140,16 @@ export default function Lab1() {
                     defaultValue="Wonderland"
                     title="The last name"
                     id="wd-text-fields-last-name" />
-                {/* copy rest of form elements here  */}
+                <h5>Text boxes</h5>
+                <label>Biography:</label><br/>
+                <textarea id="wd-textarea" cols={30} rows={10}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</textarea>
+                <h5 id="wd-buttons">Buttons</h5>
+                <button type="button"
+                        onClick={() => alert("Life is Good!")}
+                        id="wd-all-good">
+                Hello World!
+                </button>
+                
             </form>
         </div>
     </div>

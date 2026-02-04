5b93c447299512284a42798bf99453fe4dedfd0a
Olivia Hill <hill.ol@northeastern.edu>
2026-02-04 18:46:07 -0500

    bootstrap lists and forms and navigation

--- app/labs/lab2/page.tsx
+++ app/labs/lab2/page.tsx
@@ -17,6 +17,9 @@ import Container from 'react-bootstrap/Container';
 import BootstrapGrids from "./BootstrapGrids";
 import ScreenSizeLabel from "./ScreenSizeLabel";
 import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 
 export default function Lab2() {
   return (
@@ -86,6 +89,9 @@ export default function Lab2() {
         <BootstrapGrids />
         <ScreenSizeLabel />
         <BootstrapTables />
+        <BootstrapLists />
+        <BootstrapForms />
+        <BootstrapNavigation />
       </div>
     </Container>
   );

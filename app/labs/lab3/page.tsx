ed3a47e9b2681d96a70f86069b0a0d631faae926
Olivia Hill <hill.ol@northeastern.edu>
2026-02-18 10:50:07 -0500

    working on conditionals

--- app/labs/lab3/page.tsx
+++ app/labs/lab3/page.tsx
@@ -1,11 +1,16 @@
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
 
 export default function Lab3() {
   return (
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <VariablesAndConstants/>
+      <VariableTypes/>
+      <BooleanVariables/>
+      <IfElse/>
     </div>
   );
 }
